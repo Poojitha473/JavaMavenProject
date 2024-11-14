b6cea1670162589f1252c10aaa2828f73776d34a
poojitha <pothukanipoojitha@gmail.com>
2024-11-14 19:26:26 +0530

    sample maven java

---
+++ src/main/java/com/app/demo/JavaMavenProject/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.JavaMavenProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Poojitha 22BD1A055E");
+    }
+}
